69d9a7e075103db88902447046f5591bbb2d5167
MatthewAndreTaylor <matthew.andre.taylor@gmail.com>
2023-04-30 00:53:08 -0400

    Fixed typo

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def request_loader(request):
 @app.route('/update')
 @login_required
 def update():
-    origin = git.Repo('path/to/git_repo').remotes.origin
+    origin = git.Repo('https://github.com/MatthewAndreTaylor/MattBlog').remotes.origin
     origin.pull()
 
 @app.route("/login", methods=["GET", "POST"])
